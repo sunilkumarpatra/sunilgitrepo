296067ad60249d195146da3e02b8ff87667b9f7f
pkbgit <prakashkbehera@gmail.com>
2017-04-15 00:06:47 +0530

    Sunil name added to the AddTwoNumbers class

---
+++ suniljavatest/src/com/test/AddTwoNumbers.java
@@ -0,0 +1,13 @@
+package com.test;
+
+public class AddTwoNumbers {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int x = 5;
+		int y = 52;
+		int z = x + y;
+		System.out.println(z);
+	}
+
+}
